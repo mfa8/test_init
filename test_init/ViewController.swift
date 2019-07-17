bd4e05aef6613694e3844c8390ebbd68b0b796e2
Mohammed Almaroof <mohammedalmaroof33@gmail.comu>
2019-07-17 07:45:24 -0700

    adding project file

---
+++ test_init/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_init
+//
+//  Created by Mohammed Almaroof on 7/17/19.
+//  Copyright © 2019 Mohammed Almaroof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
